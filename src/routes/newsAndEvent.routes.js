7f4b5e94b64d8c4a659e8f3dada92dac77a5614a
Ssibu <sibu25456@gmail.com>
2025-08-26 13:03:49 +0530

    .

--- src/routes/newsAndEvent.routes.js
+++ src/routes/newsAndEvent.routes.js
@@ -2,10 +2,20 @@ import express from 'express';
 import { create, findAll, findOne, update, destroy, toggleStatus} from '../controllers/newsAndEvent.controller.js';
 import upload from '../middlewares/upload.js';
 const router = express.Router();
-router.post('/', upload, create);
+router.post('/', upload({
+    field: "document",
+    prefix: "event-doc",
+    uploadDir: "public/uploads/events", // Optional: Save to a specific folder
+    allowedTypes: ["image/", "application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+}), create);
 router.get('/', findAll);
 router.get('/:id', findOne);
-router.put('/:id', upload, update);
+router.put('/:id', upload({
+    field: "document",
+    prefix: "event-doc",
+    uploadDir: "public/uploads/events",
+    allowedTypes: ["image/", "application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+}), update);
 router.delete('/:id', destroy);
 router.patch('/status/:id', toggleStatus);
 
