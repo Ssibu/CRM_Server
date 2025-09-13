b2bce7fd3fc902dcf5cdfd862017742ef44fc089
Ssibu <sibu25456@gmail.com>
2025-09-13 11:05:15 +0530

    .

--- src/routes/User/BannerRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { getAllHomePageBanners } from '../../controllers/User/BannerController.js';
-
-const BannerRouter=express.Router()
-BannerRouter.get('/allbanners',getAllHomePageBanners)
-
-export default BannerRouter;
